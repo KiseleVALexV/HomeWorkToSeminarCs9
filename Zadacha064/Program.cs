d6e6fc783e9864e54e42d14c011f287c2f628572
Alexey <manoil77@list.ru>
2022-11-27 04:10:29 +0300

    Add Zadacha064

---
+++ Zadacha064/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int NumbersIn(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    Console.Write(n + " ");
+    return NumbersIn(n-1);
+}
+
+Console.WriteLine("Введите N :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int numbers = NumbersIn(n);
+Console.WriteLine(numbers);
+
+Console.ReadKey();
